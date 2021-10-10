4e3d26853e25d74324c815e8a59ffa05717bd5ad
Marcel Tode <marcel@algorithmalley.nl>
2021-10-11 00:19:21 +0200

    Backtrack on unbalanced zeros, ones

--- ouroboros.cpp
+++ ouroboros.cpp
@@ -22,7 +22,7 @@ struct puzzle_t
 {
   puzzle_t(int n) : N(n) { }
   const int N;                              // N consecutive bits (cyclic) to be unique in the total (2, N)-partition
-  const long SLEN = 1 << N;                 // The total partition length is 2^N
+  const size_t VCOUNT = 1 << N;             // The number of vertices (== num_vertices(G))
   const vertex_t VMIN = 0;                  // The pattern 00...0, i.e. N zeros
   const vertex_t VMAX = (1 << N) - 1;       // The pattern 11...1, i.e. N ones
   const vertex_t VLST = (1 << (N-1)) - 1;   // The pattern 01...1 is the LaST before cycling to VMAX again
@@ -33,11 +33,13 @@ struct state_t
 {
   state_t(puzzle_t const &ouroboros) : 
     path({ ouroboros.VMAX }), 
-    done(string(ouroboros.SLEN, '0')), 
-    count(0) { }
+    done(string(ouroboros.VCOUNT, '0')), 
+    count(0),
+    calls(0) { }
   path_t path;  // Path under construction. We start with pattern 11...1.
   string done;  // done[i] == '1' means that V_i is in P
   long count;   // nbr of solutions found
+  long calls;   // nbr of times that solve_ouroboros is entered
 };
 
 // Digraph where an edge A->B means that B is a consecutive pattern of A
@@ -63,27 +65,40 @@ auto create_graph(puzzle_t const &ouroboros) -> graph_t
   return std::move(G);
 }
 
-// Note this doesn't print all patterns in the path, because of the cyclic nature
-void print_permutation(path_t const &P, int N, long SLEN)
+// Note this doesn't emit all patterns in the path, because of the cyclic nature
+auto to_cycle(puzzle_t const &ouroboros, path_t const &P, size_t &nbr_zeros, size_t &nbr_ones) -> string
 {
-  string permutation(N, '1');
-  for (int i = 1; permutation.length() < SLEN; ++i)
+  size_t max_length = min(ouroboros.N + P.size() - 1, ouroboros.VCOUNT);
+  nbr_zeros = 0;
+  nbr_ones = ouroboros.N;
+  string cycle(ouroboros.N, '1');
+  for (int i = 1; cycle.length() < max_length; ++i)
   {
-    permutation += (P[i] & 0x01) == 1 ? "1" : "0";
+    if ((P[i] & 0x01) == 1)
+    {
+      cycle += "1";
+      nbr_ones++;
+    }
+    else
+    {
+      cycle += "0";
+      nbr_zeros++;
+    }
   }
-  cout << permutation << endl;
+  return cycle;
 }
 
 // Backtrack search for all hamiltonian cycles, starting from pattern 11...1
 void solve_ouroboros(puzzle_t const &ouroboros, graph_t const &G, state_t &state)
 {
   typename GraphTraits::out_edge_iterator out_i, out_end;
-  long SLEN = ouroboros.SLEN;
+  size_t nbr_zeros, nbr_ones;
   vertex_t v = vertex(state.path.back(), G);
-  
-  if (state.path.size() == SLEN)
+  state.calls++;
+
+  if (state.path.size() == ouroboros.VCOUNT)
   {
-    print_permutation(state.path, ouroboros.N, SLEN);
+    cout << to_cycle(ouroboros, state.path, nbr_zeros, nbr_ones) << endl;
     state.count++;
     return;
   }
@@ -91,15 +106,22 @@ void solve_ouroboros(puzzle_t const &ouroboros, graph_t const &G, state_t &state
   for (tie(out_i, out_end) = out_edges(v, G); out_i != out_end; ++out_i)
   {
     vertex_t v_out = target(*out_i, G);
-    bool is_last_too_soon = v_out == ouroboros.VLST && state.path.size() + 1 < SLEN;
+  
+    bool is_last_too_soon = v_out == ouroboros.VLST && state.path.size() + 1 < ouroboros.VCOUNT;
     bool is_already_visited = state.done[v_out] == '1';
     if (!is_last_too_soon && !is_already_visited)
     {
       state.path.push_back(v_out);
       state.done[v_out] = '1';
-
-      solve_ouroboros(ouroboros, G, state);
-
+      
+      string partial_cycle = to_cycle(ouroboros, state.path, nbr_zeros, nbr_ones);
+      bool is_unbalanced_zeros = nbr_zeros > (ouroboros.VCOUNT >> 1);
+      bool is_unbalanced_ones = nbr_ones > (ouroboros.VCOUNT >> 1);
+      if (!is_unbalanced_zeros && !is_unbalanced_ones)
+      {
+        solve_ouroboros(ouroboros, G, state);  
+      }
+      
       state.done[v_out] = '0';
       state.path.pop_back();
     }
@@ -120,6 +142,6 @@ int main(int argc, char *argv[])
   state_t state(ouroboros);
   solve_ouroboros(ouroboros, G, state);
   cout << state.count << " solutions found" << endl;
-
+  
   return 0;
 }
